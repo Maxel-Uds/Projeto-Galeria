dee6704c3c041dacf86a928b3ccf51be888d4477
Maxel-Uds <maxellopes32@gmail.com>
2022-06-26 17:53:08 -0300

    Implementando a Galeria de Fotos

--- src/js/core/includes.js
+++ src/js/core/includes.js
@@ -2,12 +2,10 @@ import $ from 'jquery';
 
 function loadIncludes(parent) {
     if(!parent) parent = 'body'
-    console.log(parent);
 
     $(parent)
         .find('[wm-include]')
         .each(function(index, element) {
-            console.log(element);
             const url = $(element).attr('wm-include');
             $.ajax({
                 url,
